ef3f7bcf0ec077c4ab66072217b4434e7ee17869
joxedanielc-PC\joxedanielc <jcastro1992@hotmail.com>
2016-11-10 22:13:47 -0500

    Cambio en el index

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -80,57 +80,47 @@
         </div>
     </section>
 
+    <?php
+    //$servername = "localhost:3306";
+    //$username = "adminE17";
+    $servername = "localhost";
+    $username = "root";
+    $password = "admin2017";
+    $dbname = "elecciones2017pc";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "SELECT * FROM candidatos";
+    $result = $conn->query($sql);
+
+    //$conn->close();
+    ?>
+    
     <!-- Contact Section -->
     <section id="contact">
         <div class="container">
-            <div class="row">
+<!--            <div class="row">
                 <div class="col-lg-12 text-center">
                     <h2>Contact Me</h2>
                     <hr class="star-primary">
                 </div>
-            </div>
+            </div>-->
             <div class="row">
-                <div class="col-lg-8 col-lg-offset-2">
-                    <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
-                    <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
-                    <form name="sentMessage" id="contactForm" novalidate>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Name</label>
-                                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Email Address</label>
-                                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Phone Number</label>
-                                <input type="tel" class="form-control" placeholder="Phone Number" id="phone" required data-validation-required-message="Please enter your phone number.">
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="row control-group">
-                            <div class="form-group col-xs-12 floating-label-form-group controls">
-                                <label>Message</label>
-                                <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <br>
-                        <div id="success"></div>
-                        <div class="row">
-                            <div class="form-group col-xs-12">
-                                <button type="submit" class="btn btn-success btn-lg">Send</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                <?php while($row = $result->fetch_assoc()) { ?>
+                    <div class="col-lg-2">
+                        <?php $rutaImagen = Yii::app()->baseUrl."/images/".$row['rutaimagen']; ?>
+                        <img src="<?php echo $rutaImagen;?>" class="img-circle" alt="" style="width: 40%; height: 40%">
+                    </div>
+                <?php }?>    
             </div>
         </div>
     </section>
+    
+    <?php
+        $conn->close();
+    ?>
